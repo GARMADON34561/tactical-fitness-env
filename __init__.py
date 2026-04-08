adc89de3de805f82ffe2b489607688c7db2e7d3c
GARMADON34561 <kanishakpatyal@gmail.com>
2026-04-08 21:03:48 +0530

    Fix inference imports, delete root __init__, use server environment

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""Tactical Fitness AI - OpenEnv Environment."""
-
-from .models import FitnessAction, FitnessObservation, FitnessState
-__all__ = ["FitnessAction", "FitnessObservation", "FitnessState"]
